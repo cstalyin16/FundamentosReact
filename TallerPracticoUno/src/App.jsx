317044b247f567b0ede5317001780b31d651ca94
Stalyin <stalyin16ac@gmail.com>
2026-07-14 20:01:29 -0500

    feat: Adding inline styling to the Movie component and using map

--- TallerPracticoUno/src/App.jsx
+++ TallerPracticoUno/src/App.jsx
@@ -5,18 +5,31 @@ function Pelicula ({titulo}){
   const [esFavorita, setEsFavorita] = useState(false);
 
   return(
-    <div onClick={()=> setEsFavorita(!esFavorita)}>
-      <p>{titulo}{esFavorita?'⭐':''}</p>
+    <div style={{
+      border:'1px solid gray',
+      borderRadius:'8px',
+      display: 'flex',
+      justifyContent: 'space-between',
+      padding: '5px',
+      margin: '8px'
+    }} 
+    onClick={()=> setEsFavorita(!esFavorita)}>
+      <p>{titulo}</p>
+      <p>{esFavorita?'⭐':''}</p>
     </div>
   )
 }
 
 function App (){
+  const pelicula = ['Moana','Dragon ball Broly','Goat','Mulan']
+  
   return (
     <>
     <h1>Mis Películas Favoritas</h1>
-
-    <Pelicula titulo={'Amazing Spider-Man 2'}></Pelicula>
+    {
+      pelicula.map((pelicula,indice)=>
+      <Pelicula key={indice} titulo={pelicula}/>)
+    }
     </>
   )
 }
