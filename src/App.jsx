7d46f739d98cdbcd525e175a8c87e78e7d99a44a
Stalyin <stalyin16ac@gmail.com>
2026-07-14 19:31:41 -0500

    feat: Agregar componente Pelicula

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
 import { useState } from 'react'
 import './App.css'
 
+function Pelicula ({titulo}){
+  return(
+    <div>
+      <p>{titulo}</p>
+    </div>
+  )
+}
+
 function App (){
   return (
     <>
     <h1>Mis Películas Favoritas</h1>
+
+    <Pelicula titulo={'Amazing Spider-Man 2'}></Pelicula>
     </>
   )
 }
