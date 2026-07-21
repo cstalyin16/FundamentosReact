ed6a96c699b273a5a46bd0ce68b26ff75efa6cba
Stalyin <stalyin16ac@gmail.com>
2026-07-21 14:21:18 -0500

    feat: add videogames data

---
+++ tienda-videojuegos/src/data/videojuegos.js
@@ -0,0 +1,44 @@
+const videojuegos = [
+  {
+    id: 1,
+    titulo: "The Legend of Zelda: Tears of the Kingdom",
+    genero: "Aventura",
+    plataforma: "Nintendo Switch",
+    lanzamiento: 2023,
+    precio: 59.99,
+    disponible: true,
+    progreso: 0.85,
+  },
+  {
+    id: 2,
+    titulo: "God of War Ragnarök",
+    genero: "Acción",
+    plataforma: "PlayStation 5",
+    lanzamiento: 2022,
+    precio: 69.99,
+    disponible: true,
+    progreso: 0.65,
+  },
+  {
+    id: 3,
+    titulo: "Forza Horizon 5",
+    genero: "Carreras",
+    plataforma: "Xbox Series X",
+    lanzamiento: 2021,
+    precio: 49.99,
+    disponible: false,
+    progreso: 0.4,
+  },
+  {
+    id: 4,
+    titulo: "Minecraft",
+    genero: "Construcción",
+    plataforma: "PC",
+    lanzamiento: 2011,
+    precio: 29.99,
+    disponible: true,
+    progreso: 1,
+  },
+];
+
+export default videojuegos;
