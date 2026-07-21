524017c18b85ad4846663c23450a200537194094
Stalyin <stalyin16ac@gmail.com>
2026-07-21 14:32:41 -0500

    feat: add the videogame table component

---
+++ tienda-videojuegos/src/components/TablaVideojuegos.jsx
@@ -0,0 +1,37 @@
+function TablaVideojuegos({ juegos }) {
+  return (
+    <div>
+      <table>
+        <thead>
+          <tr>
+            <th>Título</th>
+            <th>Género</th>
+            <th>Plataforma</th>
+            <th>Lanzamiento</th>
+            <th>Precio</th>
+            <th>Disponible</th>
+            <th>Progreso</th>
+          </tr>
+        </thead>
+        <tbody>
+          {juegos.map((juego) => (
+            <tr key={juego.id}>
+              <td>{juego.titulo}</td>
+              <td>{juego.genero}</td>
+              <td>{juego.plataforma}</td>
+              <td>{juego.lanzamiento}</td>
+              <td>{juego.precio}</td>
+              <td>{juego.disponible}</td>
+              <td>
+                <progress value={juego.progreso} max="1"></progress>
+                <span>{Math.round(juego.progreso * 100)}%</span>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default TablaVideojuegos;
