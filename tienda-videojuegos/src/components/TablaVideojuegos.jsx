7e224975684c9056c02b2a60944a3a8de8c3013b
Stalyin <stalyin16ac@gmail.com>
2026-08-13 00:25:35 -0500

    feat: Adapt inventory design to mobile devices

--- tienda-videojuegos/src/components/TablaVideojuegos.jsx
+++ tienda-videojuegos/src/components/TablaVideojuegos.jsx
@@ -20,23 +20,23 @@ function TablaVideojuegos({ juegos, onEliminar }) {
         <tbody>
           {juegos.map((juego) => (
             <tr key={juego.id}>
-              <td>{juego.titulo}</td>
-              <td>{juego.genero}</td>
-              <td>{juego.plataforma}</td>
-              <td>{juego.lanzamiento}</td>
-              <td>{juego.precio}</td>
-              <td>
+              <td data-label="Título">{juego.titulo}</td>
+              <td data-label="Género">{juego.genero}</td>
+              <td data-label="Plataforma">{juego.plataforma}</td>
+              <td data-label="Lanzamiento">{juego.lanzamiento}</td>
+              <td data-label="Precio">{juego.precio}</td>
+              <td data-label="Disponible">
                 <p className={juego.disponible ? "disponible" : "agotado"}>
                   {juego.disponible ? "Disponible" : "Agotado"}
                 </p>
               </td>
-              <td>
+              <td data-label="Progreso">
                 <div className="progreso">
                   <progress value={juego.progreso} max="1"></progress>
                   <span>{Math.round(juego.progreso * 100)}%</span>
                 </div>
               </td>
-              <td>
+              <td data-label="Acciones">
                 <div className="acciones">
                   <Link to="/editar" state={{ juego }}>
                     Editar
