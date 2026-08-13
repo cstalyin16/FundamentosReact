b6d240afc321f6648535e02e6196d6135c3c225e
Stalyin <stalyin16ac@gmail.com>
2026-08-12 23:33:01 -0500

    feat: Add delete action in the table

--- tienda-videojuegos/src/App.jsx
+++ tienda-videojuegos/src/App.jsx
@@ -4,12 +4,17 @@ import videojuegos from "./data/videojuegos";
 import TablaVideojuegos from "./components/TablaVideojuegos";
 
 function App() {
-  const [juegos] = useState(videojuegos);
+  const [juegos, setJuegos] = useState(videojuegos);
+
+  const eliminarJuego = (id) => {
+    const nuevaLista = juegos.filter((juego) => juego.id !== id);
+    setJuegos(nuevaLista);
+  };
 
   return (
     <div>
       <h2>Tienda de Videojuegos</h2>
-      <TablaVideojuegos juegos={juegos} />
+      <TablaVideojuegos juegos={juegos} onEliminar={eliminarJuego} />
     </div>
   );
 }
